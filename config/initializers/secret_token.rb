f292a06dd8c7e7ab418790f2520d01a598a90d63
Ross Ireland <rireland@cse3901-VirtualBox.(none)>
2013-12-02 14:56:07 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hangman::Application.config.secret_token = '42ad215d8db5df1c22b3bf23a47d0aef1b9240f50814360f3f3a295e295c344444b56c1950410a40ad09cd2f766b21fa30eef5d1943113395b0b3a411ec8ebab'
